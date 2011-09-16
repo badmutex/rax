ee4d6638096dd58b948fb0720a0907cf30aa7ef7
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-09-16 13:52:14 -0400

    update test using module-level pool

--- tests/trajlengths.py
+++ tests/trajlengths.py
@@ -8,11 +8,13 @@ fax.set_logging_level(fax.logging.INFO)
 
 mydirname = os.path.dirname(sys.argv[0])
 inroot    = os.path.join(mydirname, 'relaxation-data')
-nprocs    = 1
+nprocs    = 2
 pool      = fax.Pool(nprocs)
 
+fax.setup_pool(nprocs)
+
 print 'Loading project from', inroot
-project = fax.load_project(inroot, pool=pool, outputfreq=0.5)
+project = fax.load_project(inroot, outputfreq=0.5)
 # project.set_outputfrequency(0.5) # ns
 lengths = project.get_trajectory_lengths(pool=pool)
 
